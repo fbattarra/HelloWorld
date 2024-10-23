db1836f4957717ca009728883155c17371fbbc6a
Fabio <fabio.battarra@gmail.com>
2024-10-23 14:04:15 +0200

    added MyUtility > printCourse

---
+++ src/main/java/be/abis/helloworld/utility/MyUtility.java
@@ -0,0 +1,12 @@
+package be.abis.helloworld.utility;
+
+import be.abis.helloworld.model.Person;
+
+public class MyUtility {
+    public MyUtility() {
+    }
+
+    public static void printPerson(Person p){
+        System.out.println(p);
+    }
+}
